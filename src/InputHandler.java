70e5dd64d401e08553640a9fb37e2de500a326b6
Victoria Condos <vcondos@email.sc.edu>
2020-03-15 19:22:57 -0400

    Victoria first commit- created InputHandler class

---
+++ src/InputHandler.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @author Victoria Condos, Elizabeth Stevenson
+ * Purpose: Holds a hashmpa for commands and corresponding input keys
+ */
+import java.util.HashMap;
+public class InputHandler {
+	private HashMap<String, Command> commands = new HashMap<String, Command>();
+	
+}
