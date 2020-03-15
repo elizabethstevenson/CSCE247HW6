6a075eeb804e6386898950e0240e378051104562
Victoria Condos <vcondos@email.sc.edu>
2020-03-15 19:34:58 -0400

    Victoria's 2nd commit, added to InputHandler

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -7,4 +7,16 @@ import java.util.HashMap;
 public class InputHandler {
 	private HashMap<String, Command> commands = new HashMap<String, Command>();
 	
+	public void InputHandler(Document document) {
+		commands.put("load", document.load()); //TODO- I don't really understand hash maps -v
+		commands.put("save", document.save());
+		commands.put("spell", document.spell());
+		commands.put("print", document.print());
+	}
+	public void InputEntered(String data) {
+		commands.get("load");
+		commands.get("save");
+		commands.get("spell");
+		commands.get("print");
+	}
 }
