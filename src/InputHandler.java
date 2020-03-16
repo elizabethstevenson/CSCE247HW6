65678ec0e4a4f819ed273ef8dde076436665b629
brettlogeais <blogeais@email.sc.edu>
2020-03-15 20:25:15 -0400

    Removed TODOs

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -7,13 +7,13 @@ public class InputHandler {
 	private HashMap<String, Command> commands = new HashMap<>();
 	
 	public InputHandler(Document document) {
-		commands.put("load", new LoadCommand(document)); //TODO- I don't really understand hash maps -v
+		commands.put("load", new LoadCommand(document));
 		commands.put("save", new SaveCommand(document));
 		commands.put("spell", new SpellCommand(document));
 		commands.put("print", new PrintCommand(document));
 	}
 	
-	public void inputEntered(String data) { //TODO- idk when data is used
+	public void inputEntered(String data) {
 		Command command = commands.get(data);
 		if (command == null) {
 			System.out.println("Sorry, we don't recognize that command");
